e0d123a5da573ffc3a8ae85ff25edda73dcdcf2d
Vansh Jain <vanshjain25.vj@gmail.com>
2026-07-08 15:20:55 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import { app } from "./src/app.js";
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
